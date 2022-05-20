cefc65c61a9d1c1394fb95ed67b3f9c3c2ccd588
Borys De La Cruz <boryxdlc@gmail.com>
2022-05-19 20:45:32 -0500

    segundo commit

--- mvt/familia/models.py
+++ mvt/familia/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class familia(models.model):
+    profetion=(
+       ('ingeniero','ingeniero'),
+       ('medico','medico'),
+       ('profesor','profesor'),
+    )
+    date_birth = models.DateField()
+    firts_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    education = models.CharField(max_length=30, choices=profetion)
+   
